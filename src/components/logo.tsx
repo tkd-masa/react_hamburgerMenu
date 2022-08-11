461fab3aea68d913edd4b3acad3e3f491be5fd15
masahiro_takeda <m-takeda731@outlook.jp>
2022-08-11 13:18:29 +0900

    ロゴの横に会社名を追加

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,19 @@
+import styles from "styles/logo.module.css";
+
 const Logo = () => {
   return (
     <div>
-      <button>ロゴ</button>
+      <button className={styles.flexContainer}>
+        <span className={styles.logo}>
+          <img src={`${process.env.PUBLIC_URL}/images/logo.svg`} alt="" />
+        </span>
+        <span className={styles.logoText}>
+          株式会社FLAT
+          <span className={styles.logoSubText}>
+            渋谷のコーディング専門プロダクション
+          </span>
+        </span>
+      </button>
     </div>
   );
 };
